37fb5921f864236cf775e66a3083d4b60b862bda
Alan Z Chen <alanzchen7@gmail.com>
2018-12-01 11:30:43 -0500

    simplify script

--- code/combine_csvs.r
+++ code/combine_csvs.r
@@ -3,22 +3,19 @@
 library(data.table)
 library(parallel)
 library(doParallel)
+library(foreach)
+
+
 # Register half the number of cores available
 num_cores <- parallel::detectCores()
-if (num_cores > 3) {
-    cl <- parallel::makeCluster(num_cores - 1)
-    doParallel::registerDoParallel(cl)
-}
+cl <- parallel::makeCluster(num_cores - 1)
+doParallel::registerDoParallel(cl)
 
 partial_files <- system2('ls', 'data/clean/songData_*.csv', stdout=TRUE)
 
-
 # Read, clean, and compile data into one data.table
-combined <- rbindlist(
-    foreach (f=partial_files, .packages=c('data.table')) %dopar% {
+combined <- rbindlist(foreach (f=partial_files, .packages=c('data.table')) %dopar% data.table::fread(f))
+stopCluster(cl)
 
-    temp <- data.table::fread(f)
-    temp
-})
-
+# Save combined scraped csv
 write.csv(combined, file='data/songData_combined.csv', row.names=FALSE)
