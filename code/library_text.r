cbcfee475629291344ce47ecab0ca14fdba03243
Alan Z Chen <alanzchen7@gmail.com>
2018-12-04 17:08:12 -0500

    beta spread plot

--- code/library_text.r
+++ code/library_text.r
@@ -1,8 +1,9 @@
 # Library of functions for text analysis
 
 suppressMessages(library(tidyverse))
-suppressMessages(library(tidytext))
+library(tidytext)
 library(tm)
+library(topicmodels)
 
 
 # GLOBALS
@@ -27,4 +28,25 @@ count_ngrams <- function(dt, n=2, spanish=TRUE) {
         filter_at(.vars=word_vars, .vars_predicate=any_vars(!. %in% stops)) %>%
         unite(ngram, !!word_vars, sep=' ') %>%
         count(ngram, sort=TRUE)
+}
+
+
+plot_beta_spread <- function(lda, n) {
+    # Plot beta spread of lda model
+
+    topics_per_word <- tidy(lda, matrix='beta')
+
+    top_terms <- topics_per_word %>%
+        group_by(topic) %>%
+        top_n(n, beta) %>%
+        ungroup() %>%
+        arrange(topic, -beta) %>%
+        mutate(term=reorder(term, beta))
+
+    p <- ggplot(top_terms, aes(term, beta, fill=factor(topic))) +
+        geom_col(show.legend=FALSE) +
+        facet_wrap(~ topic, scale='free') +
+        coord_flip()
+
+    return(p)
 }
