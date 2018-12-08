3566224fcf48ebd5c8965042879b7d9082820a60
Alan Z Chen <alanzchen7@gmail.com>
2018-12-08 10:51:05 -0500

    update shell script

--- hpc/lda.sh
+++ hpc/lda.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --nodes=8
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=5:00:00
-#SBATCH --mem=16GB
+#SBATCH --mem=32GB
 #SBATCH --job-name=LDA
 #SBATCH --mail-type=END
 #SBATCH --mail-user=alanzchen@nyu.edu
-#SBATCH --output=slurm_%j.out
+#SBATCH --output=slurm/slurm_%j.out
   
 module purge
 module load r/intel/3.4.2
 RUNDIR=$SCRATCH/singASong/run-${SLURM_JOB_ID/.*}
 mkdir -p $RUNDIR
   
-PROJDIR=$home/azc211/singASong
+PROJDIR=home/azc211/singASong
 cd $PROJDIR
-rscript $DATADIR/code/lda.r
+rscript $PROJDIR/code/lda.r
