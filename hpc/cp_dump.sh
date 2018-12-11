1e9f6e34d2f275d9bc31b14cbef8c3c39bcc5419
Alan Z Chen <alanzchen7@gmail.com>
2018-12-11 16:55:05 -0500

    add user arg to allow file

--- hpc/cp_dump.sh
+++ hpc/cp_dump.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
 
+# User args
+FILES=$1
+
 USER=$(cat hpc/settings.csv | awk 'FNR==2 {print $2}')
-scp prince:/home/$USER/singASong/dump/* dump/
+
+scp prince:/home/$USER/singASong/dump/$FILES dump/
