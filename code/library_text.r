aa64d50969fe8cbf931e2f020ba3ad85d513e4a4
Alan Z Chen <alanzchen7@gmail.com>
2018-12-24 16:54:14 -0800

    reindent to 4 spaces

--- code/library_text.r
+++ code/library_text.r
@@ -25,33 +25,33 @@ stop_words <- rbind(stop_words,
 
 # FUNCTIONS
 register_parallel <- function(num_cores=NA) {
-  # Setup cores for parallel computing
-  
-  if (is.na(num_cores)) {
-    num_cores <- parallel::detectCores()
-  }
-  cl <- parallel::makeCluster(num_cores)
-  doParallel::registerDoParallel(cl)
-  print(paste0('Registered ', (num_cores), ' cores for parallel computing'))
-  
-  return(cl)
+    # Setup cores for parallel computing
+
+    if (is.na(num_cores)) {
+        num_cores <- parallel::detectCores()
+}
+cl <- parallel::makeCluster(num_cores)
+doParallel::registerDoParallel(cl)
+print(paste0('Registered ', (num_cores), ' cores for parallel computing'))
+
+    return(cl)
 }
 
 
 unnest_ngrams <- function(dt, n=2, english=FALSE, stopWords=stop_words) {
-  # Unnest ngrams
-  # Remove stop words
-  # Remove punctuation
+    # Unnest ngrams
+    # Remove stop words
+    # Remove punctuation
+
+    word_vars <- c(paste0('word', (1:n)))
   
-  word_vars <- c(paste0('word', (1:n)))
-  
-  if (english) {
-    stops <- stopWords %>% filter(!grepl('tm', lexicon))
-  } else {
-    stops <- stopWords
-  }
-  
-  dt <- dt %>%
+    if (english) {
+        stops <- stopWords %>% filter(!grepl('tm', lexicon))
+    } else {
+        stops <- stopWords
+    }
+
+    dt <- dt %>%
     unnest_tokens(ngram, lyrics, token='ngrams', n=n) %>%
     separate(ngram, word_vars, sep=' ') %>%
     mutate_at(.vars=word_vars, .fun=tolower) %>%
@@ -59,30 +59,30 @@ unnest_ngrams <- function(dt, n=2, english=FALSE, stopWords=stop_words) {
     mutate_at(.vars=word_vars, .funs=gsub, pattern='[[:punct:]]', replacement=NA) %>%
     drop_na(!!word_vars) %>%
     unite(ngram, !!word_vars, sep=' ')
-  
-  return(dt)
+
+    return(dt)
 }
 
 
 plot_beta_spread <- function(lda, n) {
-  # Plot beta spread of lda model
+    # Plot beta spread of lda model
+
+    topics_per_word <- tidy(lda, matrix='beta')
   
-  topics_per_word <- tidy(lda, matrix='beta')
-  
-  top_terms <- topics_per_word %>%
+    top_terms <- topics_per_word %>%
     group_by(topic) %>%
     top_n(n, beta) %>%
     ungroup() %>%
     arrange(topic, beta) %>%
     mutate(plot_order=row_number())
   
-  p <- ggplot(top_terms, aes(plot_order, beta, fill=factor(topic))) +
-    geom_col(show.legend=FALSE) +
-    facet_wrap(~ topic, scale='free') +
-    coord_flip() +
-    scale_x_continuous(breaks=top_terms$plot_order, labels=top_terms$term, expand=c(0,0)) +
-    theme_minimal() +
-    theme(axis.text=element_text(size=8))
+    p <- ggplot(top_terms, aes(plot_order, beta, fill=factor(topic))) +
+        geom_col(show.legend=FALSE) +
+        facet_wrap(~ topic, scale='free') +
+        coord_flip() +
+        scale_x_continuous(breaks=top_terms$plot_order, labels=top_terms$term, expand=c(0,0)) +
+        theme_minimal() +
+        theme(axis.text=element_text(size=8))
   
-  return(p)
+    return(p)
 }
